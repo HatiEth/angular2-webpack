62e15b883ca0cd74393d52798e7f621334a833cd
Hati Eth <hati.eth@gmail.com>
2016-06-16 09:58:33 +0200

    update package to reflect template

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+/**
+ * npm start => Target === 'start'
+ * npm run build => TARGET === 'build'
+ */
 var TARGET = process.env.npm_lifecycle_event;
 
 module.exports = require('./webpack/webpack.'+TARGET);
